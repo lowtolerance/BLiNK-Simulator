59171d6f07f91d90e204aaea1134d304c255af54
Rob Nixon <robnixon515@gmail.com>
2018-02-19 15:40:19 -0500

    added beginnings of ESP8266 IRBlaster

---
+++ IRController/IRController.ino
@@ -0,0 +1,431 @@
+#include <FS.h>                                               // This needs to be first, or it all crashes and burns
+
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+
+const char* ssid = "happywifihappywife";
+const char* password = "L0velyLady";
+const char* mqtt_server = "espie.duckdns.org";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+// User settings are below here
+
+int pinr1 = 14;                                               // Receiving pin
+int pins1 = 4;                                                // Transmitting preset 1
+
+// User settings are above here
+
+bool holdReceive = false;                                     // Flag to prevent IR receiving while transmitting
+
+IRrecv irrecv(pinr1);
+IRsend irsend1(pins1);
+
+class Code {
+  public:
+    char encoding[14] = "";
+    char address[20] = "";
+    char command[40] = "";
+    char data[40] = "";
+    String raw = "";
+    int bits = 0;
+    bool valid = false;
+};
+
+Code last_recv;
+Code last_send;
+
+//+=============================================================================
+// Reenable IR receiving
+//
+void resetReceive() {
+  if (holdReceive) {
+    Serial.println("Reenabling receiving");
+    irrecv.resume();
+    holdReceive = false;
+  }
+}
+
+//+=============================================================================
+// Setup web server and IR receiver/blaster
+//
+void setup() {
+
+  // Initialize serial
+  Serial.begin(115200);
+  Serial.println("");
+  Serial.println("ESP8266 IR Controller");
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  irsend1.begin();
+  irrecv.enableIRIn();
+  Serial.println("Ready to send and receive IR signals");
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    char receivedChar = (char)payload[i];
+    Serial.print(receivedChar);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+ // Loop until we're reconnected
+ while (!client.connected()) {
+ Serial.print("Attempting MQTT connection...");
+ // Attempt to connect
+ if (client.connect("ESP8266 Client")) {
+  Serial.println("connected");
+  // ... and subscribe to topic
+  client.subscribe("livingRoomTV");
+ } else {
+  Serial.print("failed, rc=");
+  Serial.print(client.state());
+  Serial.println(" try again in 5 seconds");
+  // Wait 5 seconds before retrying
+  delay(5000);
+  }
+ }
+}
+
+//+=============================================================================
+// Split string by character
+//
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+//+=============================================================================
+// Return which IRsend object to act on
+//
+IRsend pickIRsend (int out) {
+  switch (out) {
+    case 1: return irsend1;
+    default: return irsend1;
+  }
+}
+
+//+=============================================================================
+// Display encoding type
+//
+String encoding(decode_results *results) {
+  String output;
+  switch (results->decode_type) {
+    default:
+    case UNKNOWN:      output = "UNKNOWN";            break;
+    case NEC:          output = "NEC";                break;
+    case SONY:         output = "SONY";               break;
+    case RC5:          output = "RC5";                break;
+    case RC6:          output = "RC6";                break;
+    case DISH:         output = "DISH";               break;
+    case SHARP:        output = "SHARP";              break;
+    case JVC:          output = "JVC";                break;
+    case SANYO:        output = "SANYO";              break;
+    case SANYO_LC7461: output = "SANYO_LC7461";       break;
+    case MITSUBISHI:   output = "MITSUBISHI";         break;
+    case SAMSUNG:      output = "SAMSUNG";            break;
+    case LG:           output = "LG";                 break;
+    case WHYNTER:      output = "WHYNTER";            break;
+    case AIWA_RC_T501: output = "AIWA_RC_T501";       break;
+    case PANASONIC:    output = "PANASONIC";          break;
+    case DENON:        output = "DENON";              break;
+    case COOLIX:       output = "COOLIX";             break;
+    case GREE:         output = "GREE";               break;
+  }
+  return output;
+  if (results->repeat) Serial.print(" (Repeat)");
+}
+
+//+=============================================================================
+// Uint64 to String
+//
+String Uint64toString(uint64_t input, uint8_t base) {
+  char buf[8 * sizeof(input) + 1];  // Assumes 8-bit chars plus zero byte.
+  char *str = &buf[sizeof(buf) - 1];
+
+  *str = '\0';
+
+  // prevent crash if called with base == 1
+  if (base < 2) base = 10;
+
+  do {
+    char c = input % base;
+    input /= base;
+
+    *--str = c < 10 ? c + '0' : c + 'A' - 10;
+  } while (input);
+
+  std::string s(str);
+  return s.c_str();
+}
+
+//+=============================================================================
+// Code to string
+//
+void fullCode (decode_results *results)
+{
+  Serial.print("One line: ");
+  serialPrintUint64(results->value, 16);
+  Serial.print(":");
+  Serial.print(encoding(results));
+  Serial.print(":");
+  Serial.print(results->bits, DEC);
+  Serial.println("");
+  if (results->overflow)
+    Serial.println("WARNING: IR code too long. "
+                   "Edit IRrecv.h and increase RAWBUF");
+}
+
+//+=============================================================================
+// Code to JsonObject
+//
+void cvrtCode(Code& codeData, decode_results *results)
+{
+  strncpy(codeData.data, Uint64toString(results->value, 16).c_str(), 40);
+  strncpy(codeData.encoding, encoding(results).c_str(), 20);
+  codeData.bits = results->bits;
+  String r = "";
+  for (uint16_t i = 1; i < results->rawlen; i++) {
+    r += results->rawbuf[i] * RAWTICK;
+    if (i < results->rawlen - 1)
+      r += ",";                           // ',' not needed on last one
+    //if (!(i & 1)) r += " ";
+  }
+  codeData.raw = r;
+  if (results->decode_type != UNKNOWN) {
+    strncpy(codeData.address, ("0x" + String(results->address, HEX)).c_str(), 40);
+    strncpy(codeData.command, ("0x" + String(results->command, HEX)).c_str(), 40);
+  } else {
+    strncpy(codeData.address, "0x0", 40);
+    strncpy(codeData.command, "0x0", 40);
+  }
+}
+
+//+=============================================================================
+// Dump out the decode_results structure.
+//
+void dumpInfo(decode_results *results) {
+  if (results->overflow)
+    Serial.println("WARNING: IR code too long. "
+                   "Edit IRrecv.h and increase RAWBUF");
+
+  // Show Encoding standard
+  Serial.print("Encoding  : ");
+  Serial.print(encoding(results));
+  Serial.println("");
+
+  // Show Code & length
+  Serial.print("Code      : ");
+  serialPrintUint64(results->value, 16);
+  Serial.print(" (");
+  Serial.print(results->bits, DEC);
+  Serial.println(" bits)");
+}
+
+//+=============================================================================
+// Dump out the decode_results structure.
+//
+void dumpRaw(decode_results *results) {
+  // Print Raw data
+  Serial.print("Timing[");
+  Serial.print(results->rawlen - 1, DEC);
+  Serial.println("]: ");
+
+  for (uint16_t i = 1;  i < results->rawlen;  i++) {
+    if (i % 100 == 0)
+      yield();  // Preemptive yield every 100th entry to feed the WDT.
+    uint32_t x = results->rawbuf[i] * RAWTICK;
+    if (!(i & 1)) {  // even
+      Serial.print("-");
+      if (x < 1000) Serial.print(" ");
+      if (x < 100) Serial.print(" ");
+      Serial.print(x, DEC);
+    } else {  // odd
+      Serial.print("     ");
+      Serial.print("+");
+      if (x < 1000) Serial.print(" ");
+      if (x < 100) Serial.print(" ");
+      Serial.print(x, DEC);
+      if (i < results->rawlen - 1)
+        Serial.print(", ");  // ',' not needed for last one
+    }
+    if (!(i % 8)) Serial.println("");
+  }
+  Serial.println("");  // Newline
+}
+
+//+=============================================================================
+// Dump out the decode_results structure.
+//
+void dumpCode(decode_results *results) {
+  // Start declaration
+  Serial.print("uint16_t  ");              // variable type
+  Serial.print("rawData[");                // array name
+  Serial.print(results->rawlen - 1, DEC);  // array size
+  Serial.print("] = {");                   // Start declaration
+
+  // Dump data
+  for (uint16_t i = 1; i < results->rawlen; i++) {
+    Serial.print(results->rawbuf[i] * RAWTICK, DEC);
+    if (i < results->rawlen - 1)
+      Serial.print(",");  // ',' not needed on last one
+    if (!(i & 1)) Serial.print(" ");
+  }
+
+  // End declaration
+  Serial.print("};");  //
+
+  // Comment
+  Serial.print("  // ");
+  Serial.print(encoding(results));
+  Serial.print(" ");
+  serialPrintUint64(results->value, 16);
+
+  // Newline
+  Serial.println("");
+
+  // Now dump "known" codes
+  if (results->decode_type != UNKNOWN) {
+    // Some protocols have an address &/or command.
+    // NOTE: It will ignore the atypical case when a message has been decoded
+    // but the address & the command are both 0.
+    if (results->address > 0 || results->command > 0) {
+      Serial.print("uint32_t  address = 0x");
+      Serial.print(results->address, HEX);
+      Serial.println(";");
+      Serial.print("uint32_t  command = 0x");
+      Serial.print(results->command, HEX);
+      Serial.println(";");
+    }
+
+    // All protocols have data
+    Serial.print("uint64_t  data = 0x");
+    serialPrintUint64(results->value, 16);
+    Serial.println(";");
+  }
+}
+
+//+=============================================================================
+// Binary value to hex
+//
+String bin2hex(const uint8_t* bin, const int length) {
+  String hex = "";
+
+  for (int i = 0; i < length; i++) {
+    if (bin[i] < 16) {
+      hex += "0";
+    }
+    hex += String(bin[i], HEX);
+  }
+
+  return hex;
+}
+
+//+=============================================================================
+// Send IR codes to variety of sources
+//
+void irblast(String type, String dataStr, unsigned int len, int rdelay, int pulse, int pdelay, int repeat, long address, IRsend irsend) {
+  Serial.println("Blasting off");
+  type.toLowerCase();
+  unsigned long data = strtoul(("0x" + dataStr).c_str(), 0, 0);
+  holdReceive = true;
+  Serial.println("Blocking incoming IR signals");
+  // Repeat Loop
+  for (int r = 0; r < repeat; r++) {
+    // Pulse Loop
+    for (int p = 0; p < pulse; p++) {
+      Serial.print(data, HEX);
+      Serial.print(":");
+      Serial.print(type);
+      Serial.print(":");
+      Serial.println(len);
+      if (type == "nec") {
+        irsend.sendNEC(data, len);
+      } else if (type == "sony") {
+        irsend.sendSony(data, len);
+      } else if (type == "coolix") {
+        irsend.sendCOOLIX(data, len);
+      } else if (type == "whynter") {
+        irsend.sendWhynter(data, len);
+      } else if (type == "panasonic") {
+        Serial.print("Address: ");
+        Serial.println(address);
+        irsend.sendPanasonic(address, data);
+      } else if (type == "jvc") {
+        irsend.sendJVC(data, len, 0);
+      } else if (type == "samsung") {
+        irsend.sendSAMSUNG(data, len);
+      } else if (type == "sharpRaw") {
+        irsend.sendSharpRaw(data, len);
+      } else if (type == "dish") {
+        irsend.sendDISH(data, len);
+      } else if (type == "rc5") {
+        irsend.sendRC5(data, len);
+      } else if (type == "rc6") {
+        irsend.sendRC6(data, len);
+      } else if (type == "denon") {
+        irsend.sendDenon(data, len);
+      } else if (type == "lg") {
+        irsend.sendLG(data, len);
+      } else if (type == "sharp") {
+        irsend.sendSharpRaw(data, len);
+      } else if (type == "rcmm") {
+        irsend.sendRCMM(data, len);
+      } else if (type == "gree") {
+        irsend.sendGree(data, len);
+      }
+      if (p + 1 < pulse) delay(pdelay);
+    }
+    if (r + 1 < repeat) delay(rdelay);
+  }
+
+  Serial.println("Transmission complete");
+
+  strncpy(last_send.data, dataStr.c_str(), 40);
+  last_send.bits = len;
+  strncpy(last_send.encoding, type.c_str(), 20);
+  strncpy(last_send.address, ("0x" + String(address, HEX)).c_str(), 20);
+  last_send.valid = true;
+
+  resetReceive();
+}
+
+void loop() {
+  decode_results  results;                                        // Somewhere to store the results
+  if (!client.connected()) {
+  reconnect();
+ }
+ client.loop();
+  if (irrecv.decode(&results) && !holdReceive) {                  // Grab an IR code
+    Serial.println("Signal received:");
+    fullCode(&results);                                           // Print the singleline value
+    dumpCode(&results);                                           // Output the results as source code
+    cvrtCode(last_recv, &results);                                // Store the results
+    last_recv.valid = true;
+    Serial.println("");                                           // Blank line between entries
+    irrecv.resume();                                              // Prepare for the next value
+  }
+  delay(200);
+}
