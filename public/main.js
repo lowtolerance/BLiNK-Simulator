5a380482094c13406ee508a8e40922ab3ed4ae20
Rob Nixon <robnixon515@gmail.com>
2018-02-23 17:26:44 -0500

    Standard-ized Javascript

--- public/main.js
+++ public/main.js
@@ -1,94 +1,95 @@
-var client = new Paho.MQTT.Client("espie.duckdns.org", 1884, "client_id");
-var socket = io("192.168.1.35:5000");
+/* eslint-env Paho */
+/* eslint-env io */
+var client = new Paho.MQTT.Client('espie.duckdns.org', 1884, 'client_id')
+var socket = io('192.168.1.35:5000')
 
 socket.on('example-pong', (data) => {
-    console.log(data);
-    stringParser(data["BLiNKString"]);
-});
+  console.log(data)
+  stringParser(data['BLiNKString'])
+})
 
 window.onload = function () {
-    blocks = document.getElementsByTagName("td");
-    console.log(blocks.length);
-    for(var i=0; i< blocks.length; i++) {
-        blocks[i].addEventListener("click", bindClick(i));
-    }
+  var blocks = document.getElementsByTagName('td')
+  console.log(blocks.length)
+  for (var i = 0; i < blocks.length; i++) {
+    blocks[i].addEventListener('click', bindClick(i))
+  }
 }
 
-function bindClick(i) {
- return function(){
-          console.log("you clicked block number " + i);
-        };
+function bindClick (i) {
+  return function () {
+    console.log('you clicked block number ' + i)
+  }
 }
 
-window.addEventListener("load", function() {
-    var button = document.getElementById('hello');
+window.addEventListener('load', function () {
+  var button = document.getElementById('hello')
 
-    button.addEventListener('click', function() {
-        console.log("ping");
-        socket.emit("example-ping", { BLiNKString: '@0#0!@1#1!@2#2!@3#3!@4#4!@5#5!@6#6!@7#7!' });
-    });
-});
+  button.addEventListener('click', function () {
+    console.log('ping')
+    socket.emit('example-ping', { BLiNKString: '@0#0!@1#1!@2#2!@3#3!@4#4!@5#5!@6#6!@7#7!' })
+  })
+})
 
-client.onMessageArrived = onMessageArrived;
+client.onMessageArrived = onMessageArrived
 
 var BLiNK = {
-    "active": [],
-    "color": "#000",
-    "draw": function() {
-    td = document.getElementsByTagName("td");
+  'active': [],
+  'color': '#000',
+  'draw': function () {
+    var td = document.getElementsByTagName('td')
     for (var i = 0; i < this.active.length; i++) {
-        td[this.active[i]].setAttribute("bgcolor", this.color);
+      td[this.active[i]].setAttribute('bgcolor', this.color)
     }
-    this.active = [];
-    }
+    this.active = []
+  }
 }
 
-stringParser = function stringParser(b) {
-    var i = 0;
-    while (i < b.length) {
-        var c = b.charAt(i);
-        if (c == '@') {
-            i++;
-            c = b.charAt(i);
-            while (0 <= c && c <= 7 && i < b.length) {
-                BLiNK.active.push(c);
-                i++;
-                c = b.charAt(i);
-            }
-            i--;
-        } else if (c == '#') {
-            var colors = ["#000", "#F00", "#F53", "#FF3", "#0F0", "#00F", "#63F", "#C3F"];
-            i++;
-            BLiNK.color = colors[b.charAt(i)];
-            // -------------------
-            // | 0 | Black (off) |-------------------
-            // | 1 | Dark red    | 8 | Light red    |
-            // | 2 | Dark orange | 9 | Light orange |
-            // | 3 | Dark yellow | A | Light yellow |
-            // | 4 | Dark green  | B | Light green  |
-            // | 5 | Dark blue   | C | Light blue   |
-            // | 6 | Dark indigo | D | Light indigo |
-            // | 7 | Dark violet | E | Light violet |
-            // ------------------| F | White        |
-            //                   --------------------
-        } else if (c == '.') {
-            //Not implemented yet
-        } else if (c == '!') {
-            BLiNK.draw();
-        }
-        i++;
+var stringParser = function stringParser (b) {
+  var i = 0
+  while (i < b.length) {
+    var c = b.charAt(i)
+    if (c === '@') {
+      i++
+      c = b.charAt(i)
+      while (c >= 0 && c <= 7 && i < b.length) {
+        BLiNK.active.push(c)
+        i++
+        c = b.charAt(i)
+      }
+      i--
+    } else if (c === '#') {
+      var colors = ['#000', '#F00', '#F53', '#FF3', '#0F0', '#00F', '#63F', '#C3F']
+      i++
+      BLiNK.color = colors[b.charAt(i)]
+    // -------------------
+    // | 0 | Black (off) |-------------------
+    // | 1 | Dark red    | 8 | Light red    |
+    // | 2 | Dark orange | 9 | Light orange |
+    // | 3 | Dark yellow | A | Light yellow |
+    // | 4 | Dark green  | B | Light green  |
+    // | 5 | Dark blue   | C | Light blue   |
+    // | 6 | Dark indigo | D | Light indigo |
+    // | 7 | Dark violet | E | Light violet |
+    // ------------------| F | White        |
+    //                   --------------------
+    } else if (c === '.') {
+      // Not implemented yet
+    } else if (c === '!') {
+      BLiNK.draw()
     }
+    i++
+  }
 }
 
-function onMessageArrived(message) {
-    console.log(message.payloadString);
-    stringParser(message.payloadString);
+function onMessageArrived (message) {
+  console.log(message.payloadString)
+  stringParser(message.payloadString)
 }
 
-
-client.connect({onSuccess:onConnect});
+client.connect({onSuccess: onConnect})
 
 function onConnect () {
-    console.log("onConnect");
-    client.subscribe('livingRoomTV');
+  console.log('onConnect')
+  client.subscribe('livingRoomTV')
 }
--- server.js
+++ server.js
@@ -1,49 +1,49 @@
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-var url = require('url');
-var fs = require('fs');
-var BLiNK = require('BLiNK');
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app)
+var url = require('url')
+var fs = require('fs')
+var BLiNK = require('BLiNK')
 
-app.listen(5000);
+app.listen(5000)
 
 function handler (req, res) {
-    var path = url.parse(req.url).pathname;
+  var path = url.parse(req.url).pathname
 
-    if (path == '/') {
-        index = fs.readFile(__dirname + '/public/index.html',
-            function(error, data) {
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load index.html");
-                }
+  if (path === '/') {
+    fs.readFile(path.join(__dirname, '/public/index.html'),
+      function (error, data) {
+        if (error) {
+          res.writeHead(500)
+          return res.end('Error: unable to load index.html')
+        }
 
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
-            });
-    } else if( /\.(js)$/.test(path) ) {
-        index = fs.readFile(__dirname + '/public' + path,
-            function(error, data) {
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load " + path);
-                }
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end(data)
+      })
+  } else if (/\.(js)$/.test(path)) {
+    fs.readFile(path.join(__dirname, '/public', path),
+      function (error, data) {
+        if (error) {
+          res.writeHead(500)
+          return res.end('Error: unable to load ' + path)
+        }
 
-                res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.end(data);
-            });
-    } else {
-        res.writeHead(404);
-        res.end("Error: 404 - File not found.");
-    }
+        res.writeHead(200, {'Content-Type': 'text/plain'})
+        res.end(data)
+      })
+  } else {
+    res.writeHead(404)
+    res.end('Error: 404 - File not found.')
+  }
 }
 
 io.sockets.on('connection', function (socket) {
-    socket.on('example-ping', (data) => {
-        console.log(data);
-        BLiNK.stringParser(data["BLiNKString"]);
-        delay = 2;
-        setTimeout(function() {
-            socket.emit("example-pong", { BLiNKString: '@0#0!@1#0!@2#0!@3#0!@4#0!@5#0!@6#0!@7#0!'} );
-        }, delay*1000);
-    });
-});
+  socket.on('example-ping', (data) => {
+    console.log(data)
+    BLiNK.stringParser(data['BLiNKString'])
+    var delay = 2
+    setTimeout(function () {
+      socket.emit('example-pong', { BLiNKString: '@0#0!@1#0!@2#0!@3#0!@4#0!@5#0!@6#0!@7#0!' })
+    }, delay * 1000)
+  })
+})
